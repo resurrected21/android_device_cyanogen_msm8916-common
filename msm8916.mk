778341a9f324eebd8afcc4ea7adb7597a9afa2d4
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    msm8916-common: Add basic USB HAL that reports no status change
    Change-Id: I38bfe869f9b1b66db0e13249f65e438878d06eff

--- msm8916.mk
+++ msm8916.mk
@@ -188,7 +188,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.cyanogen_8916
 
 # Vibrator
 PRODUCT_PACKAGES += \
