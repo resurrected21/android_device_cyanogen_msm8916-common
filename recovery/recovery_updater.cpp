7d7336fc01f716d0cae7cf7af22db076b437f004
Prabhjot Singh <35163500+resurrected21@users.noreply.github.com>
2019-03-01 13:26:06 +0530

    Remove assertation

--- recovery/recovery_updater.cpp
+++ recovery/recovery_updater.cpp
@@ -34,11 +34,7 @@
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
 #define ALPHABET_LEN 256
-
-#define BASEBAND_PART_PATH "/dev/block/platform/soc.0/7824900.sdhci/by-name/modem"
-#define BASEBAND_VER_STR_START "QC_IMAGE_VERSION_STRING=MPSS.DPM."
-#define BASEBAND_VER_STR_START_LEN 33
-#define BASEBAND_VER_BUF_LEN 255
+#define KB 1024
 
 #define TZ_PART_PATH "/dev/block/platform/soc.0/7824900.sdhci/by-name/tz"
 #define TZ_VER_STR "QC_IMAGE_VERSION_STRING="
@@ -123,46 +119,6 @@ static char * bm_search(const char *str, size_t str_len, const char *pat,
     return NULL;
 }
 
-static int get_baseband_version(char *ver_str, size_t len) {
-    int ret = 0;
-    int fd;
-    int baseband_size;
-    char *baseband_data = NULL;
-    char *offset = NULL;
-
-    fd = open(BASEBAND_PART_PATH, O_RDONLY);
-    if (fd < 0) {
-        ret = errno;
-        goto err_ret;
-    }
-
-    baseband_size = lseek64(fd, 0, SEEK_END);
-    if (baseband_size == -1) {
-        ret = errno;
-        goto err_fd_close;
-    }
-
-    baseband_data = (char *) mmap(NULL, baseband_size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (baseband_data == (char *)-1) {
-        ret = errno;
-        goto err_fd_close;
-    }
-
-    /* Do Boyer-Moore search across BASEBAND data */
-    offset = bm_search(baseband_data, baseband_size, BASEBAND_VER_STR_START, BASEBAND_VER_STR_START_LEN);
-    if (offset != NULL) {
-        strncpy(ver_str, offset + BASEBAND_VER_STR_START_LEN, len);
-    } else {
-        ret = -ENOENT;
-    }
-
-    munmap(baseband_data, baseband_size);
-err_fd_close:
-    close(fd);
-err_ret:
-    return ret;
-}
-
 static int get_tz_version(char *ver_str, size_t len) {
     int ret = 0;
     int fd;
@@ -203,34 +159,6 @@ err_ret:
     return ret;
 }
 
-/* verify_baseband("BASEBAND_VERSION", "BASEBAND_VERSION", ...) */
-Value * VerifyBasebandFn(const char *name, State *state,
-                     const std::vector<std::unique_ptr<Expr>>& argv) {
-    char current_baseband_version[BASEBAND_VER_BUF_LEN];
-    int ret;
-
-    ret = get_baseband_version(current_baseband_version, BASEBAND_VER_BUF_LEN);
-    if (ret) {
-        return ErrorAbort(state, kFreadFailure, "%s() failed to read current baseband version: %d",
-                name, ret);
-    }
-
-    std::vector<std::string> args;
-    if (!ReadArgs(state, argv, &args)) {
-        return ErrorAbort(state, kArgsParsingFailure, "%s() error parsing arguments", name);
-    }
-
-    ret = 0;
-    for (auto& baseband_version : args) {
-        if (strncmp(baseband_version.c_str(), current_baseband_version, strlen(baseband_version.c_str())) == 0) {
-            ret = 1;
-            break;
-        }
-    }
-
-    return StringValue(strdup(ret ? "1" : "0"));
-}
-
 /* verify_trustzone("TZ_VERSION", "TZ_VERSION", ...) */
 Value * VerifyTrustZoneFn(const char *name, State *state,
                      const std::vector<std::unique_ptr<Expr>>& argv) {
@@ -260,6 +188,5 @@ Value * VerifyTrustZoneFn(const char *name, State *state,
 }
 
 void Register_librecovery_updater_cm() {
-    RegisterFunction("cm.verify_baseband", VerifyBasebandFn);
     RegisterFunction("cm.verify_trustzone", VerifyTrustZoneFn);
 }
